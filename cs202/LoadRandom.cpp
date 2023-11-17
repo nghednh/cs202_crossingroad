1ae8f35867c97f2696959c01663abceb13a31673
kknguyen214 <128380887+kknguyen214@users.noreply.github.com>
2023-11-17 10:03:47 +0700

    test

--- cs202/LoadRandom.cpp
+++ cs202/LoadRandom.cpp
@@ -29,23 +29,53 @@ string pos[numTitle] =
     "resource/tiles/grassFlower.jpg"
 };
 
+struct Node
+{
+    Texture t;
+    Node* next;
+};
+
+vector <Node*> arrObj;
+
+Texture _grass[6];
+
+void setTexture()
+{
+    for (int i = 0; i < 6; i++)
+        _grass[i].loadFromFile(pos[i]);
+
+    for (int i = 0; i < 11; i++)
+    {
+        Node* tmp = nullptr;
+        arrObj.push_back(tmp);
+    }
+}
+
 void loadGrass()
 {
+    setTexture();
     sf::RenderWindow window(sf::VideoMode(1280, 640), "MyProgram");
     window.setFramerateLimit(60);
     auto start = std::chrono::high_resolution_clock::now();
 
 
     Sprite grass[11];
-    Texture _grass[11];
+
     //string posGrass = "resource/tiles/grass";
 
     for (int i = 0; i < 11; i++)
     {
-        _grass[i].loadFromFile(pos[randomNum(numTitle) - 1]);
-        grass[i].setTexture(_grass[i]);
+        grass[i].setTexture(_grass[randomNum(numTitle) - 1]);
         grass[i].setPosition(Vector2f(0, i * 64));
     }
+
+    Texture obj;
+    Sprite rock;
+    obj.loadFromFile("resource/object.png");
+    rock.setTexture(obj);
+    rock.setTextureRect(sf::IntRect(18, 48, 12, 12));
+    rock.scale(5, 5);
+    rock.setPosition(0, 0);
 
     while (window.isOpen())
     {
@@ -60,14 +90,28 @@ void loadGrass()
         for (int i = 0; i < 11; i++)
         {
             if (grass[i].getPosition().y >= 639) {
-                _grass[i].loadFromFile(pos[randomNum(numTitle) - 1]);
+                grass[i].setTexture(_grass[randomNum(numTitle) - 1]);
                 grass[i].setPosition(Vector2f(0, -63));
             }
             else
+            {
                 grass[i].setPosition(Vector2f(0, (grass[i].getPosition().y + 1)));
-
+            }
+          
             window.draw(grass[i]);
         }
+
+        if (rock.getPosition().y >= 639) {
+            rock.setPosition(Vector2f(randomNum(1280 - 64), -63));
+        }
+        else
+        {
+            rock.setPosition(Vector2f(rock.getPosition().x, rock.getPosition().y + 1));
+        }
+        window.draw(rock);
+
+
+
 
         window.display();
 
